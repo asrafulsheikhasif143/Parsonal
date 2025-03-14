f81872fceda7a6aa6a667046fb4cd3333d3c470e
asrafulsheikhasif143 <168800910+asrafulsheikhasif143@users.noreply.github.com>
2025-03-15 00:34:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,29 @@ slider.addEventListener('touchend', (e) => {
 setInterval(autoSlide, 3000);
 
         
+var swiper = new Swiper('.swiper-container', {
+    loop: true,  // Infinite Loop
+    autoplay: {
+        delay: 3000, // Auto-slide every 3 seconds
+        disableOnInteraction: false,
+    },
+    effect: 'coverflow', // Smooth scrolling animation
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
